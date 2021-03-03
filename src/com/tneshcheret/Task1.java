c427f1ace073cfd0f83ee8a34265bfeb3f5666a3
Tetiana Neshcheret <neshcherett10@gmail.com>
2021-03-03 21:12:10 +0200

    Added Task 1

--- src/com/tneshcheret/Task1.java
+++ src/com/tneshcheret/Task1.java
@@ -3,7 +3,17 @@ package com.tneshcheret;
 public class Task1 {
 
     public static void main(String[] args) {
-	// Задание: Завершить пример который мы делали на занятии.
+        //   Задание: Завершить пример который мы делали на занятии.
+        Fruit greenApple = new Fruit();
+        greenApple.setColor(Color.GREEN);
+        greenApple.setType(Fruit.Type.APPLE);
+        greenApple.setPrice(25.90);
+        greenApple.setWeight(186);
+        Fruit redApple = new Fruit(Fruit.Type.APPLE, Color.RED, 146, 26.90);
+        Fruit banana = new Fruit(Fruit.Type.BANANA, Color.YELLOW, 186, 32.80);
+        Fruit lemon = new Fruit(Fruit.Type.LEMON, 246, 34.90);
 
+        double checkAmount = greenApple.calculatePrice() + redApple.calculatePrice() + banana.calculatePrice() + lemon.calculatePrice();
+        System.out.printf("Сумма чека составит %.2f грн", checkAmount);
     }
 }
